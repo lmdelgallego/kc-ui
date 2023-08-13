349215266e9028f85942ce8ad69869d396593097
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-08-13 10:42:18 -0500

    Implement Components on kclib-tester

--- projects/kclib-tester/src/app/app.module.ts
+++ projects/kclib-tester/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { KcUiModule } from 'kc-ui';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -18,6 +21,8 @@ import { Page2Component } from './pages/page2/page2.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatTableModule,
+    MatExpansionModule,
     KcUiModule,
   ],
   providers: [],
--- projects/kclib-tester/src/app/pages/home/home.component.ts
+++ projects/kclib-tester/src/app/pages/home/home.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
 
+export interface Client {
+  name: string;
+  lastName: string;
+  email: string;
+  status: string;
+}
+
+const ELEMENT_DATA: Client[] = [
+  { name: 'John', lastName: 'Doe', email: 'john@doe.com', status: 'accepted' },
+  {
+    name: 'Jack',
+    lastName: 'Makee',
+    email: 'jack@makee.com',
+    status: 'accepted',
+  },
+  {
+    name: 'Lucas',
+    lastName: 'Lopez',
+    email: 'lucas@lopez.com',
+    status: 'declined',
+  },
+  {
+    name: 'Mary',
+    lastName: 'Johnson',
+    email: 'mary@johnson.com',
+    status: 'declined',
+  },
+  {
+    name: 'June',
+    lastName: 'Mayer',
+    email: 'jun@mayer.com',
+    status: 'declined',
+  },
+  {
+    name: 'Charles',
+    lastName: 'Power',
+    email: 'charles@power.com',
+    status: 'accepted',
+  },
+];
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  displayedColumns: string[] = [
+    'name',
+    'lastName',
+    'email',
+    'status',
+    'actions',
+  ];
+  dataSource = ELEMENT_DATA;
+  filters: string[] = ['accepted', 'declined'];
 
+  constructor() {}
+
+  changeFilter = (filter: string) => {
+    console.log(filter);
+  };
 }
